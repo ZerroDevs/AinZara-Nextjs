a599a0f929ff30102d48d78b19b21067d648357f
ZeroNuxs <usamu32oooo@gmail.com>
2026-09-25 07:03:33 +0200

    chore: revert to locale as any to fix TS strict includes error

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -4,7 +4,7 @@ import {routing} from './routing';
 export default getRequestConfig(async ({requestLocale}) => {
   let locale = await requestLocale;
  
-  if (!locale || !routing.locales.includes(locale as string)) {
+  if (!locale || !routing.locales.includes(locale as any)) {
     locale = routing.defaultLocale;
   }
  
@@ -13,4 +13,5 @@ export default getRequestConfig(async ({requestLocale}) => {
     messages: (await import(`../../messages/${locale}.json`)).default
   };
 });
+
 
